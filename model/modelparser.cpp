5f0a5505fd3e8b0352aa63e44c8d4003a96bd8ce
Karlu <my@mail.com>
2015-06-09 07:02:12 +0200

    Start with modelparser algorithm

--- model/modelparser.cpp
+++ model/modelparser.cpp
@@ -20,16 +20,24 @@ namespace SpaceWitch
 	void parseToWorld(World& W)
 	{
 		
+		std::vector<ParseArg> args = parseFile("data/sector.mol");
+		Molecule* mol = parseArgList(args);
+
+
+		cout << args.size();
+
+	}
+
+	std::vector<ParseArg> parseFile(string& filename)
+	{
 		fstream file("data/sector.mol", ifstream::in);
 		string str;
-
-		Molecule* m = new Molecule();
-		Atom* a = new Atom();
 
 		std::vector<ParseArg> args;
 
 		while(getline(file, str))
 		{
+			scrapComments(str);
 			if(str.size() == 0)
 				continue;
 
@@ -44,7 +52,31 @@ namespace SpaceWitch
 			args.push_back(pa);
 		}
 
-		cout << args.size();
+		return args;
+	}
+
+	void scrapComments(string& oneline)
+	{
+		int pos = oneline.find('#');
+		oneline.erase(pos, oneline.size());
+	}
+
+	void parseArgList(vector<ParseArg>& args)
+	{
+		std::vector<Vector> vecs;
+		std::vector<Triangle> trias;
+		std::vector<Atom> atom;
+		Molecule* mol;
+
+
+		for(int i = 0; i < args.size(); ++i)
+			if (args[i].arg.size() >= 3)
+			{
+				double d1 = stod(args[i].arg[0]);
+				double d2 = stod(args[i].arg[1]);
+				double d3 = stod(args[i].arg[2]);
+			}
 
 	}
+
 }
